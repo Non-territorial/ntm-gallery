0ab5eb0de094e63516e271658f4ffbc067cbc651
nonterritorial <64267598+Non-territorial@users.noreply.github.com>
2024-12-07 21:20:10 +0100

    Add files via upload

---
+++ src/app/submit-artwork.tsx
@@ -0,0 +1,9 @@
+import SubmitArtworkForm from '@/components/SubmitArtworkForm'
+
+export default function SubmitArtworkPage() {
+  return (
+    <main>
+      <SubmitArtworkForm />
+    </main>
+  )
+}
